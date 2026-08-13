075f52e3564aa7d4573d8e0d794edec3ddfb80ab
Diego Hirigoyen <diegofedericohirigoyen@gmail.com>
2026-08-13 15:35:28 -0300

    comentar login

--- login.js
+++ login.js
@@ -1,33 +1,33 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, 
-  signInWithPopup, 
-  GoogleAuthProvider, 
-  onAuthStateChanged} 
-  from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getAuth, 
+//   signInWithPopup, 
+//   GoogleAuthProvider, 
+//   onAuthStateChanged} 
+//   from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCX7pRLWvnUrK2txyTHk_ZFE_ujmVs4HiM",
-    authDomain: "finanzas-app-af367.firebaseapp.com",
-    projectId: "finanzas-app-af367",
-    storageBucket: "finanzas-app-af367.firebasestorage.app",
-    messagingSenderId: "592476181589",
-    appId: "1:592476181589:web:fe0180b384abd0a06bf0dc"
-  };
+//   // Your web app's Firebase configuration
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyCX7pRLWvnUrK2txyTHk_ZFE_ujmVs4HiM",
+//     authDomain: "finanzas-app-af367.firebaseapp.com",
+//     projectId: "finanzas-app-af367",
+//     storageBucket: "finanzas-app-af367.firebasestorage.app",
+//     messagingSenderId: "592476181589",
+//     appId: "1:592476181589:web:fe0180b384abd0a06bf0dc"
+//   };
 
-  // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//   // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+// const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
 
-// Si el usuario YA está logueado, lo mandamos directo al dashboard (index.html)
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    window.location.href = "index.html";
-  }
-});
+// // Si el usuario YA está logueado, lo mandamos directo al dashboard (index.html)
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//     window.location.href = "index.html";
+//   }
+// });
 
 // Evento de clic en el botón de Google
 document.getElementById("btnLogin").addEventListener("click", async () => {
